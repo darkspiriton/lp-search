97e92ca2bbce44d6acecd6551aab5ab34aedc7a9
Cesar Moreno <cesar200526@gmail.com>
2019-12-20 15:00:55 -0500

    update test urls

--- js/validateLpCC.js
+++ js/validateLpCC.js
@@ -2,6 +2,9 @@ require('dotenv').config()
 let axios = require('axios')
 let jsdom = require('jsdom')
 let fs = require('fs')
+let startsWith = require('lodash/startsWith')
+let split = require('lodash/split')
+let head = require('lodash/head')
 const { JSDOM } = jsdom
 
 const getUrls = () => {
@@ -15,13 +18,42 @@ const getTextFind = () => {
   return process.env.TEXT_FIND
 }
 
-const getUrlData = async (url) => {
+const getHTMLFind = () => {
+  return process.env.HTML_FIND
+}
+
+
+const getUrlData = async (url, text) => {
   let response = await axios.get(url)
   const dom = new JSDOM(response.data)
-  // console.log(dom.window.document.querySelector("p").innerHTML)
-  let quote = dom.window.document.querySelector("p").textContent
-  // console.log()
-  return quote
+  let textInitial = head(split(text, ' ', 1))
+  let quotes = dom.window.document.querySelectorAll("p")
+  let filterQuotes = []
+  quotes.forEach(quote => {
+    let classNameParent = quote.parentElement.className
+    let valid = classNameParent.includes('text')
+    if (valid) {
+      let validText = startsWith(quote.textContent, textInitial)
+      if (validText) {
+        filterQuotes.push(quote)
+      }
+    }
+  })
+  return filterQuotes
+}
+
+const getTextContent = async (url, text) => {
+  let data = await getUrlData(url, text)
+  let texts = []
+  data.forEach(item => texts.push(item.textContent))
+  return texts
+}
+
+const getInnerHTML = async (url, text) => {
+  let data = await getUrlData(url, text)
+  let texts = []
+  data.forEach(item => texts.push(item.innerHTML))
+  return texts
 }
 
 const findDiff = (str1, str2) => {
@@ -35,9 +67,10 @@ const findDiff = (str1, str2) => {
 
 
 module.exports = {
-  getUrlData,
-  findDiff,
   getUrls,
-  getTextFind
+  getTextFind,
+  getHTMLFind,
+  getTextContent,
+  getInnerHTML
 }
 
--- test/validateLpCC.test.js
+++ test/validateLpCC.test.js
@@ -2,10 +2,20 @@ const validateLpService = require('../js/validateLpCC')
 
 const urls = validateLpService.getUrls()
 const text = validateLpService.getTextFind()
+const html = validateLpService.getHTMLFind()
 
 urls.forEach((url) => {
-  test(`Compare Comunication Consent in ${url}`, async () => {
-    let quote = await validateLpService.getUrlData(url)
-    expect(quote).toEqual(text)
+  test(`Compare Text Comunication Consent in ${url}`, async () => {
+    let quotes = await validateLpService.getTextContent(url, text)
+    quotes.forEach(quote => {
+      expect(quote).toBe(text)
+    })
+  })
+
+  test(`Compare Links Comunication Consent in ${url}`, async () => {
+    let quotes = await validateLpService.getInnerHTML(url, text)
+    quotes.forEach(quote => {
+      expect(quote).toBe(html)
+    })
   })
 })
