1c2084d8a19ee00a8e4dcc81a13a1bfc9ab4788b
Cesar Moreno <cesar200526@gmail.com>
2019-12-20 15:21:12 -0500

    update index test

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 const validateLpService = require('./validateLpCC')
+const text = validateLpService.getTextFind()
 
-console.log(validateLpService.getUrls())
+let dataResponse = async () => {
+  let data = await validateLpService.getInnerHTML('https://www.freewayinsurance.com/lp/NT082-FWYCA-A-GO-LP-E-02502/', text)
+  data.forEach(item => console.log(item))
+}
+dataResponse()
+
+// console.log(validateLpService.getUrls(text))
