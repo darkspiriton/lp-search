05cd4a45272e4fbcc772dfdee9b6b86cd4682482
Cesar Moreno <cesar200526@gmail.com>
2020-01-13 18:28:36 -0500

    update: test validateLpCC

--- test/validateLpCC.test.js
+++ test/validateLpCC.test.js
@@ -1,27 +1,29 @@
 const validateLpService = require('../js/validateLpCC')
 const urls = validateLpService.getUrls()
+let fs = require('fs')
+let data = validateLpService.getFileData();
 
 
 urls.forEach((url) => {
 
 
   it(`${url} - test_footerLegalText`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_footerLegalText')
+    let value = data[url]['test_footerLegalText']
     expect(value).toEqual(process.env.test_footerLegalText)
   })
 
   it(`${url} - test_disclaimerButtonText1`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_disclaimerButtonText1')
+    let value = data[url]['test_disclaimerButtonText1']
     expect(value).toEqual(process.env.test_disclaimerButtonText1)
   })
 
   it(`${url} - test_buttonText1`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_buttonText1')
+    let value = data[url]['test_buttonText1']
     expect(value).toEqual(process.env.test_buttonText1)
   })
 
   it(`${url} - test_disclaimer2`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_disclaimer2')
+    let value = data[url]['test_disclaimer2']
     expect(value).toEqual(process.env.test_disclaimer2)
   })
 
@@ -31,12 +33,12 @@ urls.forEach((url) => {
   })
 
   it(`${url} - test_buttonText2`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_buttonText2')
+    let value = data[url]['test_buttonText2']
     expect(value).toEqual(process.env.test_buttonText2)
   })
 
   it(`${url} - test_footerLegalText`, async () => {
-    value = await validateLpService.getUrlData(url, 'test_footerLegalText')
+    let value = data[url]['test_footerLegalText']
     expect(value).toEqual(process.env.test_footerLegalText)
   })
 
