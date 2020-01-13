650e49db4dbf0b44a0dcab4556722c7210fefa98
Cesar Moreno <cesar200526@gmail.com>
2020-01-13 18:28:06 -0500

    update: index for dataResponse test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
-const validateLpService = require('./validateLpCC')
-let ids = validateLpService.getIds()
-console.log(ids)
-
+const SERVICE = require('./findUrls')
 let dataResponse = async () => {
-  let keys = ids
-  let data = await validateLpService.getUrlDataUpdate('https://www.freewayinsurance.com/lp/NT082-FWYCA-A-GO-LP-E-02502/', keys)
+  let data = await SERVICE.getAllData()
   console.log(data)
-  // data.forEach(item => console.log(item))
 }
 dataResponse()
