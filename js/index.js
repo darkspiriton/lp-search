f5b9f473a42ec8097d2ae9f6b61fea24498e2109
Cesar Moreno <cesar200526@gmail.com>
2020-01-14 18:11:01 -0500

    update main download service

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
-const SERVICE = require('./findUrls')
-let dataResponse = async () => {
-  let data = await SERVICE.getAllData()
-  console.log(data)
+const SERVICE = require('./lpDataService')
+const LP_SERVICE = require('./lpService')
+const inquirer = require('inquirer')
+
+let dataResponse = () => {
+
+  let menu = LP_SERVICE.getMenu()
+
+  let questions = [{
+    type: 'input',
+    name: 'type',
+    default: 'default',
+    message: `Escriba tipo de test? ${menu.string}`,
+  }]
+
+  inquirer.prompt(questions).then(async (answers) => {
+    let validMenu = menu.array.includes(answers['type'])
+    if (validMenu) {
+      await SERVICE.getAllData(answers['type'])
+      console.log('------ FINISH ------')
+    } else {
+      console.log(`Opcion ${answers['type']} invalida`)
+    }
+  })
 }
+
 dataResponse()
